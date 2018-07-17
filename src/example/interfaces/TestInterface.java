d03d42e86990383a676c3e372ffb0696d3af1bd3
BlueJavaM <527431498@qq.com>
2018-07-17 15:17:39 +0800

    webservice服务端

---
+++ src/example/interfaces/TestInterface.java
@@ -0,0 +1,10 @@
+package example.interfaces;
+
+/**
+ * @Author: XieLiang
+ * @Date : 2018/7/16
+ * @Comment :
+ */
+public interface TestInterface {
+    public String getName(String userName);
+}
---
+++ src/example/interfaces/TestInterfaceImpl.java
@@ -0,0 +1,38 @@
+package example.interfaces;
+
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+
+/**
+ * @Author: XieLiang
+ * @Date : 2018/7/16
+ * @Comment :
+ */
+@WebService
+public class TestInterfaceImpl implements TestInterface {
+    @Override
+    @WebMethod
+    public String getName(String userName) {
+        System.out.println("from client:" +userName);
+        String jjSize ;
+        if("Blue".equals(userName)){
+            jjSize = "15cm";
+        }else{
+            jjSize = "1cm";
+        }
+        System.out.println("to client:" +jjSize);
+        return jjSize;
+    }
+
+    public static void main(String[] args) {
+        /** 发送webservice服务
+          * publish(String address, Object implementor)
+          * address: 服务地址
+          * implementor: 实现类
+          **/
+        Endpoint.publish("http://localhost:8081/TestService",new TestInterfaceImpl());
+    }
+}
